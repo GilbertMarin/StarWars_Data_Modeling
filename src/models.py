1a04212cc52a9f4a0972db129717271b8e557bde
Gilbert Marín <daviddlioncourt@gmail.com>
2021-04-08 22:58:04 +0000

    StarWars Data Modeling done

--- src/models.py
+++ src/models.py
@@ -9,45 +9,42 @@ from eralchemy import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True) 
     firstName = Column(String(50))
     lastName = Column(String(50))
     email = Column(String(100))
     picture = Column(String(250))
-    user = relationship('Favorite', back_populates="user") # One to Many
+    user = relationship('Favorite', back_populates="user") 
 
-class Favorite(Base):  # As every element on the list of favorites
+class Favorite(Base):  
     __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
     date = Column(String(25))
     user_id = Column(Integer, ForeignKey('user.id')) 
-    character = relationship('Character', back_populates="favorite") # One to Many
-    planet = relationship('Planet', back_populates="favorite") # One to Many
+    character = relationship('Characters', back_populates="favorite") 
+    planet = relationship('Planets', back_populates="favorite") 
 
 class Character(Base):
-    __tablename__ = 'character'
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     age = Column(Integer)
-    birthYear = Column(String(25))
     gender = Column(String(25))
-    description = Column(String(300))
+    description = Column(String())
     favorite_id = Column(Integer, ForeignKey('favorite.id'))
 
 class Planet(Base):
-    __tablename__ = 'planet'
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     population = Column(Integer)
-    terrain = Column(String(25))
-    climate = Column(String(25))
-    description = Column(String(300))
+    description = Column(String())
     favorite_id = Column(Integer, ForeignKey('favorite.id'))
 
-#------------
-#------------
-#------------
+
+
+
 
     def to_dict(self):
         return {}
